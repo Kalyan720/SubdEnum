5a27655e49de4150eb56c0c00513807576ed5cd6
Sai kalyan Kamisetti <119833878+Kalyan720@users.noreply.github.com>
2024-07-21 21:13:18 +0530

    Better readibility

--- SubdEnum.py
+++ SubdEnum.py
@@ -7,38 +7,40 @@ def subdomain_enumeration():
     Enumerate subdomains by checking predefined endpoints.
 
     Prompts the user for a base URL and checks the accessibility of predefined endpoints.
-
     """
     start_time = time.time()
     url = input("Enter the Domain that you wish to enumerate: ")
+    
     try:
         with open('subdomains-100.txt', 'r') as file:
             data = file.read().splitlines()
             total_words = len(data)
-            processed_words = 0
             subdomains_enumerated = []
+            
             with tqdm(total=total_words, desc="Checking Subdomains", unit="word") as pbar:
                 for word in data:
                     full_url = f"http://{word}.{url}"
-                    print(f"Testing {full_url}")
                     try:
                         response = requests.get(full_url, timeout=10)
                         if response.status_code == 200:
                             subdomains_enumerated.append(full_url)
-                        else:
-                            continue
                     except requests.ConnectionError:
+                        # Log connection error (e.g., domain doesn't exist)
                         pass
                     except requests.exceptions.Timeout:
+                        # Log timeout (e.g., server too slow or not responding)
                         pass
                     except requests.exceptions.RequestException as e:
-                        pass
-                    processed_words += 1
+                        # Log other request exceptions
+                        print(f"Error occurred: {e}")
+                    
                     pbar.update(1)
+            
             print("\n")
-            print(f"There are {len(subdomains_enumerated)} registered for the website")
-            for i in subdomains_enumerated:
-                print(i)
+            print(f"{len(subdomains_enumerated)} subdomains registered for the website:")
+            for subdomain in subdomains_enumerated:
+                print(subdomain)
+    
     except FileNotFoundError:
         print("Error: The subdomains file was not found.")
     except Exception as e:
@@ -46,6 +48,7 @@ def subdomain_enumeration():
     finally:
         end_time = time.time()
         run_time = end_time - start_time
-        print(f"\nRuntime: {run_time} seconds")
+        print(f"\nRuntime: {run_time:.2f} seconds")
+
 if __name__ == "__main__":
     subdomain_enumeration()
